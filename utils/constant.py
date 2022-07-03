f1056260f45d9ec7afe3de24681cb1c41626d9df
Jiatong Yu <jiatongy@princeton.edu>
2022-07-03 03:34:09 -0400

    initial

---
+++ utils/constant.py
@@ -0,0 +1,12 @@
+"""
+Constants for file paths, task names, hyperparameters. etc
+"""
+
+HOMEPATH = "Users/jiatongyu/Desktop/njfs/" #change this to your working environment 
+
+"""
+Constants related to data preprocessing
+"""
+DP_JUMP = 5
+TASK_BLACK_FRAME = "black frame"    # enable to filter out black frames 
+TASK_NO_PERSON = "no person"        # enable to filter out frame with no person 
